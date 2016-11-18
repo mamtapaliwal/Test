7da28d938d01275f1585cf2032538600012b0b47
mamtapaliwal <sharma.msgnr@gmail.com>
2016-11-18 09:24:02 -0500

    do

--- TestGit1.R
+++ TestGit1.R
@@ -1,6 +1,6 @@
 #My First Test Script
 
-<<<<<<< HEAD
+
 #my second script 2016-17-11
 
 
@@ -8,9 +8,12 @@
 
 
 #my thrird trial with git
-=======
 #My second trial with git
 
 
 # I am changing from github to see if i can pull on local
->>>>>>> 9614a35ed4e4bc0a09906b7b37869a1ad757b375
+
+# use "Raw" and save as to download a file from github
+
+# i am making changes in my downloded file locally
+
